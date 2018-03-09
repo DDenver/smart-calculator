5f290ee77fd69e07864d2374b7f74bd9a2c4b5f0
Denis <den107620@gmail.com>
2018-03-09 09:39:34 +0300

    additi7

--- src/index.js
+++ src/index.js
@@ -1,26 +1,60 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this._pow = 0; // количество pow подряд
+    this._expression = '' + initialValue; //переод в строку
+    this._namberStr = this._expression; // переменная хранящая вводимое число в строке
   }
 
-  add(number) {
-    // your implementation
+  valueOf () {
+    return eval(this._expression); // вывод 
+  } 
+
+  add(number) { // метод сложения
+    this._expression += '+' + number; // приклеивание строки
+    this._pow = 0; // счётчик pow подряд
+    this._namberStr = '' + number; // перевод числа в строку
+    return this;
   }
   
-  subtract(number) {
-    // your implementation
+  subtract(number) { // метод вычитания
+    this._expression += '-' + number; // приклеивание строки
+    this._pow = 0; // счётчик pow подряд
+    this._namberStr = '' + number; // перевод числа в строку
+    return this;
   }
 
-  multiply(number) {
-    // your implementation
+  multiply(number) { // метод УМНОЖЕНИЯ
+    this._expression += '*' + number; // приклеивание строки
+    this._pow = 0; // счётчик pow подряд
+    this._namberStr = '' + number; // перевод числа в строку
+    return this;
   }
 
-  devide(number) {
-    // your implementation
+  devide(number) { // метод деления
+    this._expression += '/' + number; // приклеивание строки
+    this._pow = 0; // счётчик pow подряд
+    this._namberStr = '' + number; // перевод числа в строку
+    return this;
   }
 
-  pow(number) {
-    // your implementation
+  pow(number) { // метод возведения в степень
+
+    if (this._pow == 0){ // если подряд нет двух и более возведений
+      let expr = this._pow + this._namberStr.length; // определяет количество символов которое нужно отрезать
+      this._expression = this._expression.slice(0, -expr); // отрезает нужное кол-во символов в главной строке
+      this._expression += 'Math.pow(' + this._namberStr + ',' + number + ')'; // добавления в основную строку 
+      this._pow++; // инкрементация к счётсику 
+    } else{ // если подряд два и более возведения
+      let expr = this._pow + this._namberStr.length; // определяет количество символов которое нужно отрезать
+      this._expression = this._expression.slice(0, -expr); // отрезает нужное кол-во символов в главной строке
+      this._expression += 'Math.pow(' + this._namberStr + ',' + number; // добавления в основную строку 
+      for (let i = 0; i <= this._pow; i++){ // цик добавления закрыввающих скобок
+        this._expression += ')';
+      }
+      this._pow++;// инкрементация к счётсику 
+    }
+    this._namberStr = '' + number; // перевод числа в строку
+    return this;
   }
 }
 
